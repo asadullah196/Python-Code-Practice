b7f47ffb9b609a77b36c3bb424a5f626c17cc864
Asadullah <md.asadullah196@gmail.com>
2020-11-01 17:13:59 +0600

    python code practice, random number generation at d999

---
+++ d999.py
@@ -0,0 +1,13 @@
+import random
+
+userName = input("Candidate Name: ")
+userFatherName = input("Father's Name: ")
+userMotherName = input("Mother's Name: ")
+dateOfBirth = input("Birth Date (DD-MM-YYYY): ")
+nID = random.randrange(1000000,100000000)
+
+print("\nGovernment Republic of Bangladesh")
+print("National ID Card")
+
+print(f"Name: {userName}\nFather's Name: {userFatherName}\nMother's Name: {userMotherName}")
+print(f"Birth Date: {dateOfBirth}\nNID No: {nID}")
